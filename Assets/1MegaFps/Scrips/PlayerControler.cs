8bebce543ff5458fa70f643d2989904ac6f0c3b3
pansekta <200703@student.pwr.wroc.pl>
2017-01-05 22:00:44 +0100

    poprawka sterowania kamery na rakiecie

--- Assets/1MegaFps/Scrips/PlayerControler.cs
+++ Assets/1MegaFps/Scrips/PlayerControler.cs
@@ -19,6 +19,7 @@ public class PlayerControler : MonoBehaviour
     //Czulość myszki (Sensitivity)
     public float czuloscMyszki = 3.0f;
     public float myszGoraDol = 0.0f;
+    public float myszLewoPrawoRakieta = 0.0f;
     //Zakres patrzenia w górę i dół.
     public float zakresMyszyGoraDol = 90.0f;
 
@@ -94,21 +95,41 @@ public class PlayerControler : MonoBehaviour
      */
     private void myszka()
     {
-        //Pobranie wartości ruchu myszki lewo/prawo.
-        // jeżeli wartość dodatnia to poruszamy w prawo,
-        // jeżeli wartość ujemna to poruszamy w lewo.
-        float myszLewoPrawo = Input.GetAxis("Mouse X") * czuloscMyszki;
-        transform.Rotate(0, myszLewoPrawo, 0);
+        if (Camera.current.name == "FootCamera")
+        {
+            print(Camera.current.name);
+            //Pobranie wartości ruchu myszki lewo/prawo.
+            // jeżeli wartość dodatnia to poruszamy w prawo,
+            // jeżeli wartość ujemna to poruszamy w lewo.
+            float myszLewoPrawo = Input.GetAxis("Mouse X")*czuloscMyszki;
+            transform.Rotate(0, myszLewoPrawo, 0);
 
-        //Pobranie wartości ruchu myszki góra/dół.
-        // jeżeli wartość dodatnia to poruszamy w górę,
-        // jeżeli wartość ujemna to poruszamy w dół.
-        myszGoraDol -= Input.GetAxis("Mouse Y") * czuloscMyszki;
+            //Pobranie wartości ruchu myszki góra/dół.
+            // jeżeli wartość dodatnia to poruszamy w górę,
+            // jeżeli wartość ujemna to poruszamy w dół.
+            myszGoraDol -= Input.GetAxis("Mouse Y")*czuloscMyszki;
 
-        //Funkcja nie pozwala aby wartość przekroczyła dane zakresy.
-        myszGoraDol = Mathf.Clamp(myszGoraDol, -zakresMyszyGoraDol, zakresMyszyGoraDol);
-        //Ponieważ CharacterController nie obraca się góra/dół obracamy tylko kamerę.
-        Camera.main.transform.localRotation = Quaternion.Euler(myszGoraDol, 0, 0);
+            //Funkcja nie pozwala aby wartość przekroczyła dane zakresy.
+            myszGoraDol = Mathf.Clamp(myszGoraDol, -zakresMyszyGoraDol, zakresMyszyGoraDol);
+            //Ponieważ CharacterController nie obraca się góra/dół obracamy tylko kamerę.
+            Camera.main.transform.localRotation = Quaternion.Euler(myszGoraDol, 0, 0);
+        }
+        else
+        {
+
+            myszLewoPrawoRakieta -= Input.GetAxis("Mouse X") * czuloscMyszki;
+
+            //Pobranie wartości ruchu myszki góra/dół.
+            // jeżeli wartość dodatnia to poruszamy w górę,
+            // jeżeli wartość ujemna to poruszamy w dół.
+            myszGoraDol -= Input.GetAxis("Mouse Y") * czuloscMyszki;
+
+            //Funkcja nie pozwala aby wartość przekroczyła dane zakresy.
+            myszGoraDol = Mathf.Clamp(myszGoraDol, -zakresMyszyGoraDol, zakresMyszyGoraDol);
+            //Ponieważ CharacterController nie obraca się góra/dół obracamy tylko kamerę.
+            Camera.main.transform.localRotation = Quaternion.Euler(myszGoraDol, -myszLewoPrawoRakieta, 0);
+        }
+        
     }
 
 }
